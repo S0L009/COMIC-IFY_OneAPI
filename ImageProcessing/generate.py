9c1da27867a795a007d3d9160e5065a93688c736
S0L009 <157977814+S0L009@users.noreply.github.com>
2024-03-28 15:18:16 +0530

    Update generate.py
    Navneet - Added a main function to make app.py access it

--- ImageProcessing/generate.py
+++ ImageProcessing/generate.py
@@ -1,6 +1,7 @@
 import cv2
 from imgtxt3gen import draw_text_on_image
 from PIL import Image
+import streamlit as st
 
 def generate_final_image(full_page_image_path, im0_path, im1_path, headings, texts, positions, font_path, font_size, font_color):
     full_page_image = cv2.imread(full_page_image_path)
@@ -28,22 +29,25 @@ def generate_final_image(full_page_image_path, im0_path, im1_path, headings, tex
 
     return final_image
 
-# Usage
-full_page_image_path = 'Templates/template1.png'
-im0_path = 'img/im4.jpg'
-im1_path = 'img/im5.jpg'
-headings = ["", ""]
-texts = [
-    "Alrighty, get ready to join me, Mr. Bean, on a wild ride through the world of Formula 1 racing! Imagine this: you've got these sleek, shiny cars that look like they've just zoomed out of one of those fancy sci-fi flicks. But nope, they're the real deal, and they're tearing up the track like nobody's business! It's like watching a bunch of cheetahs on wheels, except these cheetahs are made of metal and they go faster than you can say \"rubber chicken!\"",
-    "Now, let's chat about the drivers. These folks aren't your run-of-the-mill racers; oh no, they're like the superheroes of the racing world. They've got nerves of steel and reflexes quicker than me trying to dodge a bean-filled balloon! And boy, do they have to deal with some wacky stuff on the track – hairpin turns that'll make your stomach flip-flop and other drivers sneaking past them like they're late for a delivery of their favorite bean pizza! It's a high-octane mix of speed and strategy, folks, and let me tell you, it's more thrilling than trying to catch your hat in a gust of wind! So, next time you catch those F1 cars zooming by, remember, it's not just a race – it's a bean-tastic symphony of horsepower and adrenaline!"
-]
-positions = [(149, 600, 950, 950), (1394, 2126, 950, 950)]
-font_path = ["franklin.otf", "franklinh.otf"]
-font_size = [50, 50]
-font_color = "#48716E"
+def generate_main(im_path, texts):
+    
+    full_page_image_path = "./Templates/template1.png"
+    headings = ["", ""]
+    positions = [(149, 600, 950, 950), (1394, 2126, 950, 950)]
+    font_path = ["franklin.otf", "franklinh.otf"]
+    font_size = [50, 50]
+    font_color = "#48716E"
+    im0_path = im_path[0]
+    if len(im_path) == 1: 
+        im1_path = r"C:\Users\Krish\OneDrive - Amrita Vishwa Vidyapeetham\Comic-ify\comic.jpg"
+        texts.append("Why Comic-IFY? Comic-ify isn't just about making content pretty; it's about making it accessible. We believe that knowledge should be a joy to consume, not a burden to bear. With Comic-ify, you'll not only understand the content better, but you'll actually enjoy the process. It's a win-win situation.")
+    else:
+        im1_path = im_path[1]
 
-final_image = generate_final_image(full_page_image_path, im0_path, im1_path, headings, texts, positions, font_path, font_size, font_color)
+    final_image = generate_final_image(full_page_image_path, im0_path, im1_path, headings, texts, positions, font_path, font_size, font_color)
+    final_image = cv2.cvtColor(final_image, cv2.COLOR_BGR2RGB)
+    return final_image
 
-fim = Image.fromarray(cv2.cvtColor(final_image, cv2.COLOR_BGR2RGB))
-fim.show()
-cv2.imwrite("final_image.jpg", final_image)
+
+if __name__ == "__main__":
+    generate_main()
