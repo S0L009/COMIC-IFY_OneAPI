39a138b2568834f341fda9c8b4d1f394d36f9a78
Amrit Subramanian <77322873+macromrit@users.noreply.github.com>
2024-03-29 16:27:32 +0530

    Update main.py

--- fast-api-implementation/main.py
+++ fast-api-implementation/main.py
@@ -21,12 +21,12 @@ class Chunk(BaseModel):
     chunk_content: str
 
 
-@app.post('/gen-image') # just the host's name for routing
+@app.post('/gen-image', tags=["Image Generation"]) # just the host's name for routing
 def gen_img(img: Image): # giving query parameters "localhost/blog?par1=val&par2=val"
     json_b64_img = final_wrapper_for_gen_image(img_gen_chain, img.theme)
     return {'json-b64-format-of-image-generated': json_b64_img}
 
-@app.post('/gen-chunk') # just the host's name for routing
+@app.post('/gen-chunk', tags=["Text Fantasization"]) # just the host's name for routing
 def gen_chunk(chunk: Chunk): # giving query parameters "localhost/blog?par1=val&par2=val"
     fantasized_chunk = final_wrapper_for_chunk_fantasization(chunk_fantasy_chain, chunk.theme, chunk.word_limit, chunk.chunk_content)
     print("Im here")
